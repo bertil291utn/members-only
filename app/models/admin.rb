8a63d58cf43d1c31d45613014aee5d52f176ef4f
Meme <meme_es@hotmail.com>
2020-05-19 15:07:51 -0600

    Change the login method using email instead username
    This commit changes the login method using the e-mail of the user insted
    the username.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,5 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
+         :recoverable, :rememberable, :validatable, authentication_keys: [:email]
 end
